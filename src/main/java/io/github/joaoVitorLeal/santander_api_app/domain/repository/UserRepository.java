b3dc0c1e48eeb8ba4d9a82bd775d5735439665dd
joaoVitorLeal <joaoleal98@outlook.com>
2025-07-24 18:58:43 -0300

    feat: add methods

--- src/main/java/io/github/joaoVitorLeal/santander_api_app/domain/repository/UserRepository.java
+++ src/main/java/io/github/joaoVitorLeal/santander_api_app/domain/repository/UserRepository.java
@@ -3,5 +3,13 @@ package io.github.joaoVitorLeal.santander_api_app.domain.repository;
 import io.github.joaoVitorLeal.santander_api_app.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+
+    boolean existsByCpf(String cpf);
+
+    Optional<User> findByCpf(String cpf);
 }
