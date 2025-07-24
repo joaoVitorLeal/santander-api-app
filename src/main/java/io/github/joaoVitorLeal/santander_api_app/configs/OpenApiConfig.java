a75fdfa1b3e33821319e1fe9974523815f282a53
joaoVitorLeal <joaoleal98@outlook.com>
2025-07-24 19:03:10 -0300

    feat: add OpenAPI configuration with Swagger documentation

---
+++ src/main/java/io/github/joaoVitorLeal/santander_api_app/configs/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package io.github.joaoVitorLeal.santander_api_app.configs;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Santander Open Academy — GoF with Spring Boot — REST API Demonstration",
+                version = "1.1.0",
+                description = """
+            This project demonstrates  developed as part of the DIO and Santander Open Academy initiative.
+            Source code: * Not yet available *
+            """,
+                termsOfService = "https://opensource.org/licenses/MIT",
+                license = @License(
+                        name = "MIT License",
+                        url = "https://opensource.org/licenses/MIT"
+                ),
+                contact = @Contact(
+                        name = "João Leal",
+                        email = "joaoleal98@outlook.com",
+                        url = "https://github.com/joaoVitorLeal"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "LinkedIn — Professional Profile",
+                url = "https://linkedin.com/in/joaovlc"
+        )
+)
+public class OpenApiConfig {
+}
