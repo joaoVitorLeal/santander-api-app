84de98d915bd990017c859c600f5d8e47a59dfd6
joaoVitorLeal <joaoleal98@outlook.com>
2025-07-24 18:12:19 -0300

    feat: implement user service layer

---
+++ src/main/java/io/github/joaoVitorLeal/santander_api_app/service/UserService.java
@@ -0,0 +1,17 @@
+package io.github.joaoVitorLeal.santander_api_app.service;
+
+import io.github.joaoVitorLeal.santander_api_app.domain.model.User;
+import io.github.joaoVitorLeal.santander_api_app.dtos.UserRequestDTO;
+import io.github.joaoVitorLeal.santander_api_app.dtos.UserResponseDTO;
+
+import java.util.List;
+
+public interface UserService {
+
+    UserResponseDTO findById(Long id);
+    List<UserResponseDTO> findAll();
+    void create(UserRequestDTO userDto);
+    User createAndReturnUserEntity(UserRequestDTO userDto);
+    void update(Long id, UserRequestDTO userDto);
+    void delete(Long id);
+}
---
+++ src/main/java/io/github/joaoVitorLeal/santander_api_app/service/impl/UserServiceImpl.java
@@ -0,0 +1,123 @@
+package io.github.joaoVitorLeal.santander_api_app.service.impl;
+
+import io.github.joaoVitorLeal.santander_api_app.domain.exceptions.AccountNumberAlreadyExistsException;
+import io.github.joaoVitorLeal.santander_api_app.domain.exceptions.CpfAlreadyRegisteredException;
+import io.github.joaoVitorLeal.santander_api_app.domain.exceptions.UserNotFoundException;
+import io.github.joaoVitorLeal.santander_api_app.domain.model.Account;
+import io.github.joaoVitorLeal.santander_api_app.domain.model.User;
+import io.github.joaoVitorLeal.santander_api_app.domain.repository.UserRepository;
+import io.github.joaoVitorLeal.santander_api_app.domain.validator.UserValidator;
+import io.github.joaoVitorLeal.santander_api_app.dtos.UserRequestDTO;
+import io.github.joaoVitorLeal.santander_api_app.dtos.UserResponseDTO;
+import io.github.joaoVitorLeal.santander_api_app.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repository;
+    private final UserValidator validator;
+
+    public UserServiceImpl(UserRepository repository, UserValidator validator) {
+        this.repository = repository;
+        this.validator = validator;
+    }
+
+    @Override
+    public UserResponseDTO findById(Long id) {
+        return repository.findById(id)
+                .map(UserResponseDTO::new)
+                .orElseThrow(() -> new UserNotFoundException("Not found user with ID: " + id));
+    }
+
+    @Override
+    public List<UserResponseDTO> findAll() {
+        return repository.findAll()
+                .stream()
+                .map(UserResponseDTO::new)
+                .toList();
+    }
+
+    @Override
+    public void create(UserRequestDTO userToCreate) {
+        if (repository.existsByCpf(userToCreate.cpf())) {
+            throw new CpfAlreadyRegisteredException(userToCreate.cpf());
+        }
+        if (repository.existsByAccountNumber(userToCreate.account().number())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        repository.save(userToCreate.toEntity(userToCreate));
+    }
+
+    @Override
+    public User createAndReturnUserEntity(UserRequestDTO userToCreate) {
+        if (repository.existsByAccountNumber(userToCreate.account().number())) {
+            throw new AccountNumberAlreadyExistsException("accountNumber", "The Account number '" + userToCreate.account().number() + "' already exists.");
+        }
+        return repository.save(userToCreate.toEntity(userToCreate));
+    }
+
+    @Override
+    public void update(Long id, UserRequestDTO userToUpdate) {
+        User existingUser = repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("To update, the user must already be registered in the database."));
+
+        existingUser.setName(userToUpdate.name());
+        existingUser.setCpf(userToUpdate.cpf());
+        existingUser.setAccount(userToUpdate.account().toEntity());
+
+        validator.validate(existingUser);
+        repository.save(existingUser);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.findById(id)
+                .ifPresentOrElse(
+                        repository::delete,
+                        () -> {
+                            throw new UserNotFoundException("Not found user with ID: " + id);
+                        }
+                );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
