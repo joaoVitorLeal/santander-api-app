942d67826c07d8d6fa86052db5399c9364000022
joaoVitorLeal <joaoleal98@outlook.com>
2025-07-25 14:09:21 -0300

    refactor: update UserService to extend generic CrudService

--- src/main/java/io/github/joaoVitorLeal/santander_api_app/service/UserService.java
+++ src/main/java/io/github/joaoVitorLeal/santander_api_app/service/UserService.java
@@ -1,19 +1,7 @@
 package io.github.joaoVitorLeal.santander_api_app.service;
 
-import io.github.joaoVitorLeal.santander_api_app.domain.model.User;
 import io.github.joaoVitorLeal.santander_api_app.dtos.UserRequestDTO;
 import io.github.joaoVitorLeal.santander_api_app.dtos.UserResponseDTO;
 
-import java.util.List;
-
-public interface UserService {
-    UserResponseDTO findById(Long id);
-
-    List<UserResponseDTO> findAll();
-
-    User create(UserRequestDTO userDto);
-
-    void update(Long id, UserRequestDTO userDto);
-
-    void delete(Long id);
+public interface UserService extends CrudService<UserRequestDTO, UserResponseDTO, Long> {
 }
