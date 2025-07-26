ae385fe218beb3aacdd393c21df12d33be007bf8
joaoVitorLeal <joaoleal98@outlook.com>
2025-07-25 21:02:06 -0300

    fix: Fix user field bug mapping in UserMapper#toEntity method

--- src/main/java/io/github/joaoVitorLeal/santander_api_app/mappers/UserMapper.java
+++ src/main/java/io/github/joaoVitorLeal/santander_api_app/mappers/UserMapper.java
@@ -1,15 +1,31 @@
 package io.github.joaoVitorLeal.santander_api_app.mappers;
 
-import io.github.joaoVitorLeal.santander_api_app.domain.model.*;
+import io.github.joaoVitorLeal.santander_api_app.domain.model.User;
 import io.github.joaoVitorLeal.santander_api_app.dtos.UserRequestDTO;
 import io.github.joaoVitorLeal.santander_api_app.dtos.UserResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
-@Mapper(componentModel = "spring", uses = {Account.class, Card.class, Feature.class, News.class})
+@Mapper(
+        componentModel = "spring",
+        uses = {
+                AccountMapper.class,
+                CardMapper.class,
+                FeatureMapper.class,
+                NewsMapper.class
+        }
+)
 public interface UserMapper {
 
-
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "account.id", ignore = true),
+            @Mapping(target = "card.id", ignore = true),
+            @Mapping(target = "features[].user", ignore = true),
+            @Mapping(target = "news[].user", ignore = true)
+    })
     User toEntity(UserRequestDTO dto);
 
-    UserResponseDTO toDTO(User user);
+    UserResponseDTO toDTO(User entity);
 }
