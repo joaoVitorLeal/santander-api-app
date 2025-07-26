afdf4df31c8381bc452ea6edc88446693765aa00
joaoVitorLeal <joaoleal98@outlook.com>
2025-07-25 21:14:38 -0300

    feat: Add CPF format verification

--- src/main/java/io/github/joaoVitorLeal/santander_api_app/dtos/UserRequestDTO.java
+++ src/main/java/io/github/joaoVitorLeal/santander_api_app/dtos/UserRequestDTO.java
@@ -4,12 +4,13 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 
 public record UserRequestDTO(
         @NotBlank String name,
-        @NotBlank String cpf,
+        @NotBlank @Pattern(regexp = "\\d{11}", message = "The CPF must contain eleven (11) digits.") String cpf,
         @NotNull @Valid AccountDTO account,
         @Valid CardDTO card,
         @NotEmpty @Valid List<FeatureDTO> features,
