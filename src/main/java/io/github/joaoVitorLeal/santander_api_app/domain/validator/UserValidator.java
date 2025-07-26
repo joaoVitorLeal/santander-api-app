b2909f6c19f9e842b0b6689165f60a5f308fe0e3
joaoVitorLeal <joaoleal98@outlook.com>
2025-07-25 21:11:50 -0300

    fix: resolve bug validations and add new card number validation method that throws the CardNumberAlreadyExistsException

--- src/main/java/io/github/joaoVitorLeal/santander_api_app/domain/validator/UserValidator.java
+++ src/main/java/io/github/joaoVitorLeal/santander_api_app/domain/validator/UserValidator.java
@@ -1,11 +1,11 @@
 package io.github.joaoVitorLeal.santander_api_app.domain.validator;
 
+import io.github.joaoVitorLeal.santander_api_app.domain.exceptions.AccountNumberAlreadyExistsException;
+import io.github.joaoVitorLeal.santander_api_app.domain.exceptions.CardNumberAlreadyExistsException;
 import io.github.joaoVitorLeal.santander_api_app.domain.exceptions.CpfAlreadyRegisteredException;
 import io.github.joaoVitorLeal.santander_api_app.domain.model.User;
 import io.github.joaoVitorLeal.santander_api_app.domain.repository.UserRepository;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Component
 public class UserValidator {
@@ -17,20 +17,33 @@ public class UserValidator {
     }
 
     public void validate(User userToValidate) {
-        if (isUserRegistered(userToValidate)) {
-            throw new CpfAlreadyRegisteredException(userToValidate.getCpf());
-        }
+        validateCpf(userToValidate);
+        validateAccountNumber(userToValidate);
+        validateCardNumber(userToValidate);
     }
 
-    private boolean isUserRegistered(User userToValidate) {
-        Optional<User> possibleUser = repository.findByCpf(userToValidate.getCpf());
+    private void validateCpf(User userToValidade) {
+        repository.findByCpf(userToValidade.getCpf()).ifPresent(existingUser -> {
+            if (!existingUser.getId().equals(userToValidade.getId())) {
+                throw new CpfAlreadyRegisteredException(userToValidade.getCpf());
+            }
+        });
+    }
 
-        if (userToValidate.getId() == null) {
-            return possibleUser.isPresent();
-        }
+    private void validateAccountNumber(User userToValidade) {
+        repository.findByAccountNumber(userToValidade.getAccount().getNumber())
+                .ifPresent(existingUser -> {
+            if (!existingUser.getId().equals(userToValidade.getId())) {
+                throw new AccountNumberAlreadyExistsException(userToValidade.getAccount().getNumber());
+            }
+        });
+    }
 
-        return possibleUser
-                .map(foundUser -> !foundUser.getId().equals(userToValidate.getId())) // true -> se possuir ID's diferentes mas mesmo CPF (registro duplicado)
-                .orElse(false);
+    private void validateCardNumber(User userToValidade) {
+        repository.findByCardNumber(userToValidade.getCard().getNumber()).ifPresent(existingUser -> {
+            if (!existingUser.getId().equals(userToValidade.getId())) {
+                throw new CardNumberAlreadyExistsException(userToValidade.getCard().getNumber());
+            }
+        });
     }
 }
