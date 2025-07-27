49aac29774ec5fb252164b9806527557b668f871
joaoVitorLeal <joaoleal98@outlook.com>
2025-07-27 15:17:41 -0300

    Bugfix: Display Swagger datas

--- src/main/java/io/github/joaoVitorLeal/santander_api_app/configs/OpenApiConfig.java
+++ src/main/java/io/github/joaoVitorLeal/santander_api_app/configs/OpenApiConfig.java
@@ -5,16 +5,18 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
+        servers = { @Server(url = "/", description = "Default server URL") }, // CORS definition for Swagger
         info = @Info(
                 title = "Santander Open Academy — GoF with Spring Boot — REST API Demonstration",
                 version = "1.1.0",
                 description = """
             This project demonstrates  developed as part of the DIO and Santander Open Academy initiative.
-            Source code: * Not yet available *
+            Source code: https://github.com/joaoVitorLeal/santander-api-app
             """,
                 termsOfService = "https://opensource.org/licenses/MIT",
                 license = @License(
